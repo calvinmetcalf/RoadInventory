04e3f20f48aec8d5f43d00424943d7f0d1b55a4a
Calvin Metcalf <calvin.metcalf@state.ma.us>
2012-11-19 09:47:07 -0500

    updates

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-var L = this.L;
+function toGeoJSON(k,l){function m(a){if(1===a.rings.length)return{type:"Polygon",coordinates:a.rings};var b,a=a.rings;b=[];for(var c=[],e=a.length,g=0;e>g;){for(var h=a[g],i=h.length-1,f=0,d=0;i>f;)d+=h[f][0]*h[f+1][1]-h[f+1][0]*h[f][1],f++;0>=d?b.push(a[g]):c.push(a[g]);g++}b=[b,c];a=b[0];c=b[1];b=[];if(0===c.length){for(c=a.length;0<c;)b.push([a[0]]);return{type:"MultiPolygon",coordinates:b}}return 1===a.length?(c.unshift(a[0]),{type:"Polygon",coordinates:c}):{type:"MultiPolygon",coordinates:a,
+holes:c}}for(var i={type:"FeatureCollection",features:[]},n=k.features.length,d=0;n>d;){var a=k.features[d],e={type:"Feature",properties:a.attributes};if(a.geometry.x)e.geometry={type:"Point",coordinates:[a.geometry.x,a.geometry.y]};else if(a.geometry.points){var j=e;a=a.geometry;a=1===a.points.length?{type:"Point",coordinates:a.points[0]}:{type:"MultiPoint",coordinates:a.points};j.geometry=a}else a.geometry.paths?(j=e,a=a.geometry,a=1===a.paths.length?{type:"LineString",coordinates:a.paths[0]}:{type:"MultiLineString",
+coordinates:a.paths},j.geometry=a):a.geometry.rings&&(e.geometry=m(a.geometry));i.features.push(e);d++}if(l)l(i);else return i};
 L.Util.jsonp = function (url, cb, cbParam, callbackName){
     var cbn,ourl,cbs;
     var cbParam = cbParam || "callback";
@@ -8,7 +10,7 @@ L.Util.jsonp = function (url, cb, cbParam, callbackName){
         cbs = "_" + Math.floor(Math.random()*1000000);
         cbn = "L.Util.jsonp.cb." + cbs;
     }
-    L.Util.jsonp.cb = {};
+    
     L.Util.jsonp.cb[cbs] = cb;
     var scriptNode = L.DomUtil.create('script','', document.getElementsByTagName('body')[0] );
     scriptNode.type = 'text/javascript';
@@ -19,6 +21,7 @@ L.Util.jsonp = function (url, cb, cbParam, callbackName){
     }
     scriptNode.src = ourl;   
 };
+L.Util.jsonp.cb = {};
 var m = L.map('map').setView([42.2, -71], 8),
 	popUp,
     h = new L.Hash(m),
@@ -50,7 +53,8 @@ var overlayMaps = {
 };
 var lc=L.control.layers(baseMaps, overlayMaps);
 lc.addTo(m);
-m.on("click", getInfo);
+var clickable = L.geoJson("",{style:{opacity:0}}).addTo(m)
+clickable.on("click", getInfo);
 function getInfo(e){
     function getLayer(z){
      return (z-4)*2;   
@@ -72,5 +76,21 @@ function getInfo(e){
 		}
 		var popUpcontent = L.Util.template("<div><ol><li>{el}</li></ol></div>",{el: out.join("</li><li>")});
 		popUp = L.popup().setLatLng(latlng).setContent(popUpcontent).openOn(m);
+	});
+}
+m.on("moveend", getClickable);
+getClickable();
+function getClickable(){
+    function getLayer(z){
+     return (z-4)*2;   
+    }
+    var b = m.getBounds();
+    var zoom = m.getZoom();
+    var urlT = "http://services.massdot.state.ma.us/ArcGIS/rest/services/RoadInventory/Roads/MapServer/{layer}/query?geometry={bounds}&geometryType=esriGeometryEnvelope&inSR=4326&spatialRel=esriSpatialRelContains&returnGeometry=true&outSR=4326&f=json";
+	var urlOpt = {layer: getLayer(zoom), bounds:b.toBBoxString()};
+	var url = L.Util.template(urlT, urlOpt);
+	L.Util.jsonp(url,function (data){
+		clickable.clearLayers();
+		toGeoJSON(data,function(g){clickable.addData(g);});
 	});
 }
